721f1b669b1edf5cb60cdfba5fd712a304a0ac0c
adamhendrickson22 <adamhendrickson22@gmail.com>
2024-11-13 09:04:33 -0500

    Example Vending Machine marker on map
    I hope you can easily add more markers with specified JS functions. It'll suck if not.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,23 @@
-let name = "cool guys";
-function greet(){
-    console.log("Hello " + name);
+async function initMap() {
+    const contentString = `
+          <div>
+            <h1>Example Vending Machine</h1>
+            <div>
+              <p>
+                This is an example Vending Machine.
+              </p>
+            </div>
+          </div>`;
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: contentString,
+        ariaLabel: "",
+    });
+
+    const marker = document.querySelector('gmp-advanced-marker');
+    marker.addEventListener('gmp-click', () => {
+        infoWindow.open({anchor: marker});
+    });
 }
-greet();
+
+window.initMap = initMap;
